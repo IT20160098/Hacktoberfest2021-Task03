7d0dd61d7fe87bc25d7a071cf1ad5d979960ae85
Amasha Sathsarani <86179556+IT20160098@users.noreply.github.com>
2021-10-08 23:49:35 +0530

    Create Insert Doctor Details.java

---
+++ Insert Doctor Details.java
@@ -0,0 +1,43 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Doctor_Insert")
+public class Doctor_Insert extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String name = request.getParameter("fname")
+		String gender = request.getParameter("gender");
+		String email = request.getParameter("iemail");
+		String phone = request.getParameter("mobile");
+		string hospital = request.getParameter("hospital");
+		String specialty = request.getParameter("specialty");
+		
+		boolean isdoctor
+		
+		isdoctor = DoctorDBUtil.InsertDoctor(name, gender, email, phone, hospital, specialty);
+		
+		if(isdoctor == true) 
+			RequestDispatcher rd = request.getRequestDispatcher("success.jsp");
+			rd.forward(request, response)
+		}
+		else {
+			RequestDispatcher rd2 = request.getRequestDispatcher("unsuccess.jsp")
+			rd2.forward(request, response);
+		}
+		 
+	}
+
+}
